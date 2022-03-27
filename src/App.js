480d608c15ec69d4528aeeeccedbc436a56b697f
Obaid Ur Rehman <obaidkhan972@gmail.com>
2022-03-27 23:56:42 +0500

    Added env & pusher

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import ChatBar from "./components/chatbar/ChatBar";
 import SideBar from "./components/sidebar/SideBar";
+import Pusher from 'pusher-js'
 import './components/styles/app.css'
 
 function App() {
+  var pusher = new Pusher(process.env.REACT_APP_PUSHER_TOKEN, {
+    cluster: 'ap2'
+  });
+
+  var channel = pusher.subscribe('messages');
+  channel.bind('inserted', function(data) {
+    alert(JSON.stringify(data));
+  });
   return (
     <div className="app">
       <div className="app_window">
